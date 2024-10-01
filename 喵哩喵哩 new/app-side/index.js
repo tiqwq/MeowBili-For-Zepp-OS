c7f918fd8ceb6c3e1fb54f9915b267e7c8fc4ffd
StrGin <166109147+StrGin@users.noreply.github.com>
2024-10-01 10:19:16 +0800

    Add files via upload

--- 喵哩喵哩 new/app-side/index.js
+++ 喵哩喵哩 new/app-side/index.js
@@ -1,141 +1,98 @@
 import { BaseSideService } from "@zeppos/zml/base-side";
-import { encWbi , InitBiliWbi } from '../utils/wbi.js'
+import { encWbi, InitBiliWbi } from '../utils/wbi.js';
+
 const BILI_BASE_HEADERS = {
-  "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/127.0.0.0",
-  "TE": "trailers",
-  "Referer": "https://www.bilibili.com",
-  "Origin": "https://www.bilibili.com",
-  "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-}
-async function fetchData(res) {
-  try {
-    const response = await fetch('https://passport.bilibili.com/x/passport-login/web/qrcode/generate', {
-      method: 'GET'
-    });
-    const resBody = await response.json();
-    console.log(resBody);
-    res(null, {
-      result: [resBody.data.url, resBody.data.qrcode_key],
-    });
-  } catch (error) {
-    res(null, { 
-      result: "ERROR",
-    });
-  }
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0',
+  'Referer': 'https://www.bilibili.com',
+  'Origin': 'https://www.bilibili.com',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+  'Accept-Encoding': '',
+  'Accept-Language': 'zh-CN,zh;q=0.8',
 };
 
-async function checkQRStatus(res, qr_key) {
-  console.log(qr_key);
-    const response = await fetch(`https://passport.bilibili.com/x/passport-login/web/qrcode/poll?qrcode_key=${qr_key}`);
-    console.log(response.body.data.url);
+function parseHTMLToJSON(html) {
+  const title = (html.match(/<title[^>]*>([^<]*)<\/title>/i) || [])[1]?.trim().split(' - 哔哩哔哩')[0].trim() || '';
+  const author = (html.match(/<meta[^>]*name="author"[^>]*content="([^"]*)"/i) || [])[1]?.trim() || '';
+  const publishTime = (html.match(/<span[^>]*class="publish-text"[^>]*>([^<]*)<\/span>/i) || [])[1]?.trim() || '';
+  const articleContent = (html.match(/<div[^>]*id="article-content"[^>]*>([\s\S]*?)<\/div>/i) || [])[1]?.replace(/<[^>]+>/g, '').replace(/\s+/g, ' ').trim() || '';
 
-    res(response)
-}; 
-async function SendBiliGET(res,type,data,url){
-  var headers = BILI_BASE_HEADERS;
-  headers["Cookie"] =  "DedeUserID=" + data.DedeUserID +  
-  "; DedeUserID__ckMd5=" + data.DedeUserID__ckMd5 + 
-  "; SESSDATA=" + data.SESSDATA + 
-  "; bili_jct=" + data.bili_jct + 
-  "; buvid3=" + data.buvid3;
-  console.log("DedeUserID=" + data.DedeUserID +  
-  "; DedeUserID__ckMd5=" + data.DedeUserID__ckMd5 + 
-  "; SESSDATA=" + data.SESSDATA + 
-  "; bili_jct=" + data.bili_jct + 
-  "; buvid3=" + data.buvid3);
-  var ret = await fetch(url,
-    {
-      method: 'GET',
-      responseType: type,
-      headers: headers  
-    }
-    );
-    console.log(url);
-    
-    console.log(ret);
-    res(ret);
-} 
-async function SendBiliPOST(res,type,data,url,content_type,parameters){
-  var headers = BILI_BASE_HEADERS;
-  headers["Cookie"] =  "DedeUserID=" + data.DedeUserID +  
-  "; DedeUserID__ckMd5=" + data.DedeUserID__ckMd5 + 
-  "; SESSDATA=" + data.SESSDATA + 
-  "; bili_jct=" + data.bili_jct + 
-  "; buvid3=" + data.buvid3;
-  headers["Content-Type"] = "application/x-www-form-urlencoded";
-  console.log("DedeUserID=" + data.DedeUserID +  
-  "; DedeUserID__ckMd5=" + data.DedeUserID__ckMd5 + 
-  "; SESSDATA=" + data.SESSDATA + 
-  "; bili_jct=" + data.bili_jct + 
-  "; buvid3=" + data.buvid3);
-  var ret = await fetch(url,
-    {
-      method: 'POST',
-      responseType: type,
-      headers: headers,
-      // body: parameters,
-      credentials: 'include'
-    }
-    );
-    console.log(url);
-    console.log(ret);
-    res(ret);
-} 
-async function SendWbiGET(res,url, data,type, paramsobj,info){
-  console.log(info);
-  console.log(info.body.data.face);
-  let init = InitBiliWbi(info.body.data.wbi_img)
-  var headers = BILI_BASE_HEADERS;
-  headers["Cookie"] =  
-  "DedeUserID=" + data.DedeUserID +  
-  "; DedeUserID__ckMd5=" + data.DedeUserID__ckMd5 + 
-  "; SESSDATA=" + data.SESSDATA + 
-  "; bili_jct=" + data.bili_jct + 
-  "; buvid3=" + data.buvid3;
-  url = url + "?" + encWbi(paramsobj, init[0], init[1])
-  console.log("WbiGet URL: " + url)
-  var ret = await fetch({
-      url: url,
-      responseType: type,
-      headers: headers
+  return {
+    title,
+    author,
+    article: {
+      content: articleContent,
+      publishTime,
+    },
+  };
+}
+
+async function getQRcode(res) {
+  try {
+    const response = await fetch('https://passport.bilibili.com/x/passport-login/web/qrcode/generate', { method: 'GET' });
+    const resBody = await response.json();
+    res(null, { result: [resBody.data.url, resBody.data.qrcode_key] });
+  } catch {
+    res(null, { result: "ERROR" });
+  }
+}
+
+async function checkQRStatus(res, qr_key) {
+  const response = await fetch(`https://passport.bilibili.com/x/passport-login/web/qrcode/poll?qrcode_key=${qr_key}`);
+  res(response);
+}
+
+async function sendBiliRequest(res, method, url, data, type, contentType, parameters) {
+  const headers = { ...BILI_BASE_HEADERS, Cookie: `DedeUserID=${data.DedeUserID}; DedeUserID__ckMd5=${data.DedeUserID__ckMd5}; SESSDATA=${data.SESSDATA}; bili_jct=${data.bili_jct}; buvid3=${data.buvid3}` };
+  if (contentType) headers['Content-Type'] = contentType;
+
+  const response = await fetch(url, {
+    responseType: 'json',
+    method,
+    headers
   });
-  console.log(ret);
-  res(ret)
+
+  res(response);
+  console.log(response);
+  console.log(url)
 }
-async function DownloadImage(url,filename,targetName) {
-  network.downloader.downloadFile({
-    url: url,
-    timeout: 60000,
-    filePath: 'data://download/' + filename
-  })
-  image
-  .convert({
-    filePath: 'data://download/test.png',
-    targetFilePath: 'data://download/' + targetName
-  })
-  .then((result) => {
-    console.log(result.targetFilePath) // data://download/converted_test.pang
-  })
+
+async function sendWbiRequest(res, method, url, data, type, paramsobj, info) {
+  const init = InitBiliWbi(info);
+  const headers = { ...BILI_BASE_HEADERS, Cookie: `DedeUserID=${data.DedeUserID}; DedeUserID__ckMd5=${data.DedeUserID__ckMd5}; SESSDATA=${data.SESSDATA}; bili_jct=${data.bili_jct}; buvid3=${data.buvid3}` };
+  url += `?${encWbi(paramsobj, init[0], init[1])}`;
+
+  const response = await fetch(url, {
+    method,
+    headers,
+    credentials: 'include',
+  });
+
+  res(response);
+  console.log(response);
+  console.log(url)
 }
-AppSideService( 
+
+async function downloadImage(url, filename, targetName) {
+  await network.downloader.downloadFile({ url, timeout: 60000, filePath: `data://download/${filename}` });
+  await image.convert({ filePath: `data://download/${filename}`, targetFilePath: `data://download/${targetName}` });
+}
+
+AppSideService(
   BaseSideService({
     onInit() {},
 
     onRequest(req, res) {
-      console.log("=====>,", req.method);
-      if (req.method === "GET_DATA") {
-        fetchData(res);
-      } else if (req.method === "CHECK_QR_STATUS") {
-        checkQRStatus(res, req.data.qr_key);
-      } else if (req.method == "SENDBILIGET") {
-        SendBiliGET(res,req.type,req.data,req.url)
-      } else if (req.method == "SENDWBIGET") {
-        SendWbiGET(res,req.url,req.data,req.type,req.paramsobj,req.info)
-      } else if (req.method == "DOWNLOADIMAGE") {
-        DownloadImage(req.url,req.filename,req.targetName)
-      } else if (req.method == "SENDBILIPOST") {
-        SendBiliPOST(res,req.type,req.data,req.url,req.content_type,req.parameters)
+      const { method, data, type, url, paramsobj, info, content_type, parameters, id, filename, targetName } = req;
+
+      switch (method) {
+        case "GET_QR": getQRcode(res); break;
+        case "CHECK_QR_STATUS": checkQRStatus(res, data.qr_key); break;
+        case "SENDBILIGET": sendBiliRequest(res, 'GET', url, data, type); break;
+        case "SENDWBIGET": sendWbiRequest(res, 'GET', url, data, type, paramsobj, info); break;
+        case "DOWNLOADIMAGE": downloadImage(url, filename, targetName); break;
+        case "SENDBILIPOST": sendBiliRequest(res, 'POST', url, data, type, content_type, parameters); break;
+        case "SENDWBIPOST": sendWbiRequest(res, 'POST', url, data, type, paramsobj, info); break;
+        case "GETCOLUMNS": fetch(`https://www.bilibili.com/read/cv${id}/`).then(response => response.text()).then(html => res(parseHTMLToJSON(html))); break;
       }
     },
 
