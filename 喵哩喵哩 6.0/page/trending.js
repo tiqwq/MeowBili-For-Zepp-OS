d39e20ad19f39c5b4725e4722054ff01e81a2e89
yiaolin <88192574+tiqwq@users.noreply.github.com>
2025-01-12 03:06:40 +0800

    Add files via upload
    全新的搜索页面，为每一个热榜配上一个填充矩形背板，热榜间距离变大易于查看

--- 喵哩喵哩 6.0/page/trending.js
+++ 喵哩喵哩 6.0/page/trending.js
@@ -8,19 +8,103 @@ let buttonList = []
 Page(
   BasePage({
     build() {
+      createWidget(widget.IMG, {
+          x: 0,
+          y: 0,
+          src: 'Bg.png'
+        });
       for(let i = 0; i < 10; i++) {
+        
+        buttonList[i] = createWidget(widget.FILL_RECT,{ 
+          x: 35, 
+          y: 310 + i * 130, 
+          w: 420, 
+          h: 80, 
+          color:  0x222222, 
+          radius: 30
+         })
         buttonList[i] = createWidget(widget.TEXT, {
           x: 50,
-          y: 170 + i * 70,
+          y: 330 + i * 130,
           w: 420,
           h: 180,
           text_size: 22,
-          text: "",
+          text: "🔥"+"",
+          align_h: align.CENTER_H,
+
           color: 0xffffff,
         })
       }
-         this.getTrending() 
-        createWidget(widget.BUTTON, {
+         this.getTrending()  
+         createWidget(widget.IMG, {
+          x: 190,
+          y: 50,
+          src: "back.png",
+        })
+        createWidget(widget.TEXT, {
+          x: 220,
+          y: 40,
+          w: px(245),
+          h: px(88),
+          text_size: 32,
+          text: "搜索",
+          color: 0xffffff
+        })
+        createWidget(widget.IMG, {
+          x: 80,
+          y: 196,
+          src: "about_code.png",
+        })
+        createWidget(widget.TEXT, {
+          x: 130,
+          y: 190,
+          w: 400,
+          h: 88,
+          text_size: 15,
+          text: "我们为您提供了一个全新的中文输入法\n以便您更好地搜索您内心所想",
+          color: 0xffffff
+        })
+         createWidget(widget.IMG, {
+          x: 100,
+          y: 100,
+          src: "searchbg.png",
+        }).addEventListener(event.CLICK_UP, () => {
+          push({
+            url: "page/board",
+            params: "searchresult"
+          })
+        });
+        createWidget(widget.IMG, {
+          x: 125,
+          y: 125,
+          src: "search.png",
+        }); createWidget(widget.TEXT, {
+          x: 210,
+          y: 247,
+          w: px(245),
+          h: px(88),
+          text_size: 32,
+          text: "🔥热榜🔥",
+          color: 0xffffff
+        })
+        createWidget(widget.TEXT, {
+          x: 100,
+          y: 120,
+          w: 288,
+          h: 46,
+          color: 0xffffff,
+          text_size: 20,
+          align_h: align.CENTER_H,
+          align_v: align.CENTER_V,
+          text_style: text_style.NONE,
+          text: '点击以搜索',
+        }).addEventListener(event.CLICK_UP, () => {
+          push({
+            url: "page/board",
+            params: "searchresult"
+          })
+        });
+       /*  createWidget(widget.BUTTON, {
           x: 60,
           y: 70,
           w: px(360),
@@ -36,7 +120,7 @@ Page(
               params: "searchresult"
             })
           }
-        }) 
+        })  */
     },
     getTrending() {
       getTrending(this).then((res) => {
